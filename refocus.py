a8a5724cad3b2f0fb8e559c034e107b6f69b34da
max <mprosk@gmail.com>
2024-07-10 22:15:47 -0400

    Minor improvements

--- refocus.py
+++ refocus.py
@@ -31,7 +31,7 @@ def get_file_list(target_path: Path) -> List[Path]:
     return file_list
 
 
-def process_file(source_dir: Path, source_file: Path, output_dir: Path) -> Path:
+def process_file(source_dir: Path, source_file: Path, output_dir: Path):
     filename = clean_filename(source_file.name)
     output_path = output_dir / source_file.relative_to(source_dir).parent
     copy_path = output_path / (filename + ".wav")
@@ -42,11 +42,20 @@ def process_file(source_dir: Path, source_file: Path, output_dir: Path) -> Path:
         copy_path = output_path / f"{filename}_{i}.wav"
         i += 1
 
-    print(source_file, "-->", copy_path)
+    # Check for overly long filenames
+    if len(copy_path.name) > 255:
+        print("WARNING: long filename:", source_file, "-->", copy_path)
+
+    # print(source_file, "-->", copy_path)
     copy_path.parent.mkdir(parents=True, exist_ok=True)
     # shutil.copy2(source_file, copy_path)
-    subprocess.run(['ffmpeg', '-i', str(source_file), '-ar', '48000', '-sample_fmt', 's16', str(copy_path)],
-                   check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    try:
+        subprocess.run(['ffmpeg', '-i', str(source_file), '-ar', '48000', '-sample_fmt', 's16', str(copy_path)],
+                       check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError as ex:
+        print("ERROR:", source_file, "-->", copy_path)
+        print(str(ex))
+        return None
     return copy_path
 
 
@@ -57,15 +66,16 @@ def clean_filename(filename: str) -> str:
     i = tokens.index("mp3")
     filename = ".".join(tokens[:i])
 
-    # Remove preceding underscores
-    while filename.startswith('_'):
-        filename= filename[1:]
+    # Remove preceding non-alphanumeric characters
+    while not filename[0].isalnum():
+        filename = filename[1:]
 
     # Handle a blank filename
     if not filename:
         return "_"
 
     return filename
+
 
 def main(source: str, output: str):
     source_path = Path(source)
@@ -79,12 +89,22 @@ def main(source: str, output: str):
     # Create the output directory if it does not exist
     output_path.mkdir(parents=True, exist_ok=True)
 
+    error_list = []
+
     with open(output_path / "refocus.log", mode='w') as log:
         for i, file_path in enumerate(file_list):
             resolved_path = process_file(source_path, file_path, output_path)
-            log.write(f"{file_path} --> {resolved_path}\n")
+            if resolved_path is not None:
+                log.write(f"{file_path} --> {resolved_path}\n")
+            else:
+                error_list.append(file_path)
             if i % 100 == 0:
                 print(i, "of", len(file_list), f"({round(i / len(file_list) * 100)}%)")
+
+    if error_list:
+        print("Errors were encountered on the following files:")
+        for p in error_list:
+            print(p)
 
 
 if __name__ == '__main__':
